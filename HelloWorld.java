724ce2c82971e10c7dc859ba52d65680d7e55a21
Beatrice <oprea.beatrice@yahoo.com>
2019-11-06 21:14:17 +0200

    added message to test committing a new version

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,9 @@
-public class  HelloWorld {	
+public class  HelloWorld {
 	public static void main(String[] args) {	
 	System.out.println("Hello World!");
-	}
 
+System.out.println("testing a new version");
+System.out.println("the 2nd version");
+
+	}
 }
